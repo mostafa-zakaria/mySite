7832f213694bdddc338a936d750266c54eed3b59
MZX <mostafa.zakaria@gmail.com>
2016-08-07 20:52:31 +0100

    Database Integration
    Used the default SQLite Database to provide the Disease Names instead of
    Hardcoding them in an array

--- main_app/models.py
+++ main_app/models.py
@@ -6,6 +6,10 @@ from django.db import models
 class Disease(models.Model):
 	name = models.CharField(max_length = 30)
 	pain = models.CharField(max_length = 30)
+	cure = models.CharField(max_length = 100)
 	survivability = models.IntegerField()
+
+	def __str__(self):
+                return(self.name)
 
 		
